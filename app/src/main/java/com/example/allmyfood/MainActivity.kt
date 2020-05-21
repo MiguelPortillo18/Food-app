8325f02d93c8ec4df0415b6da0ef65dd7b1ec106
Walter26 <walterrafael26@gmail.com>
2020-05-20 21:03:54 -0600

    Update repo

--- app/src/main/java/com/example/allmyfood/MainActivity.kt
+++ app/src/main/java/com/example/allmyfood/MainActivity.kt
@@ -2,25 +2,32 @@ package com.example.allmyfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 
 class MainActivity : AppCompatActivity() {
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var signInButton = findViewById<SignInButton>(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD)
+        signInButton.setColorScheme(1)
+
         var gso: GoogleSignInOptions =
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).requestEmail()
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail()
                 .build();
 
-        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     override fun onSupportNavigateUp(): Boolean {
