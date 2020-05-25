834347a6a6244b9b5b1aea496fe6353ea8bf240e
Walter26 <walterrafael26@gmail.com>
2020-05-25 13:26:25 -0600

    Reorder files

---
+++ Application/app/src/main/java/com/example/allmyfood/ExploreFragment.kt
@@ -0,0 +1,26 @@
+package com.example.allmyfood
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ExploreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_explore)
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+
+}
---
+++ Application/app/src/main/java/com/example/allmyfood/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.allmyfood
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_home.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        setContentView(R.layout.activity_main)
+        findViewById<SignInButton>(R.id.sign_in_button).setOnClickListener(onClick)
+        findViewById<TextView>(R.id.tv_register).setOnClickListener(onClickRegisterTv)
+        findViewById<Button>(R.id.btn_login).setOnClickListener(onClickBtnLogin)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.aboutFragment -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var account = GoogleSignIn.getLastSignedInAccount(this)
+
+        var signInButton = findViewById<SignInButton>(R.id.sign_in_button)
+        signInButton.setSize(SignInButton.SIZE_STANDARD)
+    }
+
+    private var onClick = View.OnClickListener{
+        when(it.id) {
+            R.id.sign_in_button -> signIn()
+        }
+    }
+
+    private var onClickRegisterTv = View.OnClickListener {
+        var intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    private var onClickBtnLogin = View.OnClickListener {
+        var intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun signIn(){
+        var signInIntent = mGoogleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0){
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>){
+        try{
+            var account = task.getResult(ApiException::class.java)
+        }
+        catch(e: ApiException){
+
+        }
+    }
+}
